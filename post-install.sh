dafee1abc9f8252cb07ffa7972fac404af7ab35c
nani8ot <github@wiedemer.xyz>
2023-03-15 00:28:09 +0100

    test post install; disable flatpak update timer

--- post-install.sh
+++ post-install.sh
@@ -3,8 +3,10 @@
 set -ouex pipefail
 
 systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
+#systemctl enable flatpak-system-update.timer
 
 systemctl --global enable flatpak-user-update.timer
+
+systemctl disable flatpak-system-update.timer
 
 cp /usr/share/ublue-os/ublue-os-update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
