6a9b126c657ba06d65f29bc03519cb73a04f7437
nani8ot <github@wiedemer.xyz>
2023-03-15 00:46:44 +0100

    add flathub remote

--- post-install.sh
+++ post-install.sh
@@ -3,10 +3,10 @@
 set -ouex pipefail
 
 systemctl enable rpm-ostreed-automatic.timer
-#systemctl enable flatpak-system-update.timer
+systemctl enable flatpak-system-update.timer
 
 systemctl --global enable flatpak-user-update.timer
 
-systemctl disable flatpak-system-update.timer
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 cp /usr/share/ublue-os/ublue-os-update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
