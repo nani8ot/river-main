d454464f99461b8bae81001087b5307f02292457
nani8ot <github@wiedemer.xyz>
2023-03-16 03:39:15 +0100

    use systemd presets

--- post-install.sh
+++ post-install.sh
@@ -2,13 +2,16 @@
 
 set -ouex pipefail
 
-systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
+#systemctl enable rpm-ostreed-automatic.timer
+#systemctl enable flatpak-system-update.timer
 
-systemctl --global enable flatpak-user-update.timer
+#systemctl --global enable flatpak-user-update.timer
 
 # mask rfkill and ppd for tlp to work
-systemctl mask systemd-rfkill.service systemd-rfkill.socket power-profiles-daemon.service
-systemctl enable tlp.service
+#systemctl mask systemd-rfkill.service systemd-rfkill.socket power-profiles-daemon.service
+#systemctl enable tlp.service
+
+systemctl preset-all
+systemctl --user preset-all
 
 cp /usr/share/ublue-os/ublue-os-update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
